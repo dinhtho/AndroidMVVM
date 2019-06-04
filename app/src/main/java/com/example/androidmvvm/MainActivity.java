600a352dc9268cce0e09abec3f9f467ce37d391e
dinhtho <tho.nguyen@beesightsoft.com>
2019-06-04 20:00:47 +0700

    add data binding

--- app/src/main/java/com/example/androidmvvm/MainActivity.java
+++ app/src/main/java/com/example/androidmvvm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidmvvm;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.androidmvvm.databinding.ActivityMainBinding;
 import com.example.androidmvvm.viewmodel.CounterViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,17 +22,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bt = findViewById(R.id.bt);
-        tvCount = findViewById(R.id.tv_count);
+//        setContentView(R.layout.activity_main);
+//        bt = findViewById(R.id.bt);
+//        tvCount = findViewById(R.id.tv_count);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+
 
         CounterViewModel counterViewModel = ViewModelProviders.of(this)
                 .get(CounterViewModel.class);
         counterViewModel.getCounter().observe(this, count -> {
-            tvCount.setText(String.valueOf(count.getCount()));
+            binding.setCount(count);
         });
 
-        bt.setOnClickListener((v) -> {
+        binding.bt.setOnClickListener((v) -> {
             counterViewModel.onButtonClick();
         });
 
